c3924033cac65c357c335dec2ce440cea3b442c9
Dmitry Mazin <dm@archvile.net>
2021-09-19 10:47:45 -0400

    Revert "Remove api/cmd stuff"
    This reverts commit 4ac43116404ef2a531b05fe83b8d1478abffa79c.

---
+++ api/http/handle_set.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type setHandler struct {
+	db KVSetter
+}
+
+func (h *setHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+	value := r.URL.Query().Get("value")
+
+	err := h.db.Set(key, value)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("error: error setting key=`%s`,value=`%s`: %s", key, value, err)))
+
+		return
+	}
+
+	fmt.Fprintf(w, "key=%s,value=%s", key, value)
+}
